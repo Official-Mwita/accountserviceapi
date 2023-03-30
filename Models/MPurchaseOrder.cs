bee4e2240780c3432c1ac61c8494f42ede442e99
jaykaranja <mainakaranja2001@gmail.com>
2023-03-30 14:53:36 +0300

    Removed some requirements causing some conflict

--- Models/MPurchaseOrder.cs
+++ Models/MPurchaseOrder.cs
@@ -6,23 +6,14 @@ namespace BookingApi.Models
 {
     public class MPurchaseOrder: iPurchaseOrder
     {
-        [Required]
         public string CostCenter { get; set; } = string.Empty;
-        [Required]
         public string ShipsTo { get; set; } = string.Empty;
-        [Required]
         public int OrderAmount { get; set; }
-        [Required]
         public DateTime FirstDeliveryDate { get; set; }
-        [Required]
         public string Narration { get; set; } = string.Empty;
-        [Required]
         public string Supplier { get; set; } = string.Empty;
-        [Required]
         public DateTime OrderDate { get; set; }
-        [Required]
         public int DeliveryPeriod { get; set; }
-        [Required]
         public string VehicleDetails { get; set; } = string.Empty;
         public int OrderNumber { get; set; }
         public string id {get; set;} = string.Empty;
